aa49283dba3236511705657415724220a925ba59
AoZ112 <scotrhodesjunior@10.23.1.58>
2018-06-19 09:27:56 +0200

    changed ManageGods

--- Runescape/src/business/ManageGods.java
+++ Runescape/src/business/ManageGods.java
@@ -1,6 +1,19 @@
 package business;
 
-public class ManageGods {
+import java.util.ArrayList;
 
+public class ManageGods implements IManageGods{
+	
+	@Override
+	public void searchGod() {
+		String searchStringGod = "";
+		ArrayList<God> gods = new ArrayList<God>();
+		
+		
+	}
 
+	@Override
+	public void filterGod() {
+		ArrayList<God> gods = new ArrayList<God>();
+	}
 }

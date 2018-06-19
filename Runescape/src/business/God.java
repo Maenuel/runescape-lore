33a2856cb49e0ca9f111b57021dfa4e4f4567bed
Maenuel <manuel.wuersten@gibmit.ch>
2018-06-19 08:31:29 +0200

    add God and Tier

---
+++ Runescape/src/business/God.java
@@ -0,0 +1,76 @@
+package business;
+
+public class God {
+	int age;
+	String era;
+	String colors;
+	String ascension;
+	String gender;
+	String birthRace;
+	String name;
+	String description;
+	String image;
+	int	tierID;
+	
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEra() {
+		return era;
+	}
+	public void setEra(String era) {
+		this.era = era;
+	}
+	public String getColors() {
+		return colors;
+	}
+	public void setColors(String colors) {
+		this.colors = colors;
+	}
+	public String getAscension() {
+		return ascension;
+	}
+	public void setAscension(String ascension) {
+		this.ascension = ascension;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirthRace() {
+		return birthRace;
+	}
+	public void setBirthRace(String birthRace) {
+		this.birthRace = birthRace;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public int getTierID() {
+		return tierID;
+	}
+	public void setTierID(int tierID) {
+		this.tierID = tierID;
+	}
+}
---
+++ Runescape/src/business/Tier.java
@@ -0,0 +1,18 @@
+package business;
+
+public class Tier {
+	String description;
+	int ID;
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getID() {	
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+}
